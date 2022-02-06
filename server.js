723a0b97c70666b6cc0d5933d8bd7cbb345d6804
Clfreeman22 <clfreeman22@gmail.com>
2022-02-06 07:19:57 -0600

    removed develop file

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const path = require('path');
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Allows use of files within 'public' directory that are mentioned
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on PORT ${PORT}!`);
+});
