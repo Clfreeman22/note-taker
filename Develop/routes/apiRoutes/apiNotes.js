b82c5bf07d634362aa927e64c4b3bc44b67198c6
Clfreeman22 <clfreeman22@gmail.com>
2022-02-06 07:01:25 -0600

    added routes

--- Develop/routes/apiRoutes/apiNotes.js
+++ Develop/routes/apiRoutes/apiNotes.js
@@ -1,3 +1,31 @@
 const { createNewNote, validateNote, deleteNote } = require('../../lib/notes');
 let { notes }  = require('../../db/db.json');
 const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+    console.log(results);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+router.delete('/notes/:id', (req, res) => {
+    let singleNoteId = req.params.id;
+    notes = notes.filter(note => note.id !== singleNoteId);
+
+    deleteNote(notes);
+    res.json(notes);
+
+});
+
+module.exports = router;
